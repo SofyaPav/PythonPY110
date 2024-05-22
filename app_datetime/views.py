6b1714a6f2162582cad64fdd22e476b7daa4d1f9
Sofya <sofyapavlova12@gmail.com>
2024-05-22 21:32:41 +0300

    1

---
+++ app_datetime/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from datetime import datetime
+
+
+def datetime_view(request):
+    if request.method == "GET":
+        data = datetime.now()
+        return HttpResponse(data)
