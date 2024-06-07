1f6c6c571cf156f7553de2b50b6557e7fd941422
sofpav <sofyapavlova12@gmail.com>
2024-06-07 20:55:17 +0300

    Лабораторная 3, задание 3 сделано(практика 5)

--- store/views.py
+++ store/views.py
@@ -23,3 +23,21 @@ def shop_view(request):
         with open('store/shop.html', encoding="utf-8") as f:
             data = f.read()  # Читаем HTML файл
         return HttpResponse(data)  # Отправляем HTML файл как ответ
+
+
+def products_page_view(request, page):
+    if request.method == "GET":
+        if isinstance(page, str):
+            for data in DATABASE.values():
+                if data['html'] == page:
+                    with open(f'store/products/{page}.html', encoding="utf-8") as f:
+                        data = f.read()
+                    return HttpResponse(data)
+        elif isinstance(page, int):
+            data = DATABASE.get(str(page))  # Получаем какой странице соответствует данный id
+            if data:  # Если по данному page было найдено значение
+                with open(f'store/products/{data["html"]}.html', encoding="utf-8") as f:
+                    data = f.read()
+                return HttpResponse(data)
+
+        return HttpResponse(status=404)
